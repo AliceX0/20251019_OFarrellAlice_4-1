a405aac9dcf7550810fe3f4359c118be8512e5c1
Alice O'Farrell <AliceOFarrell@Outlook.com>
2025-10-19 16:23:17 -0400

    I'm getting confident with using my own solutions! Even if the changes are tiny.

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-"""
-This program calculates prices for custom house signs.
-"""
+charge = 35.00
+numChars = int(input("How many characters are on the sign?: "))
+woodType = str(input("Is the sign made of pine or oak?: "))
+color = str(input("Is the text black or gold?: "))
 
-# Declare and initialize variables here.
-# Charge for this sign.
-# Number of characters.
-# Color of characters.
-# Type of wood.
+if numChars > 5: 
+    charge = charge + 4 * (numChars - 5)
+   
+if woodType == "pine":
+    charge = charge
+if woodType == "oak":
+    charge = charge + 20.00
 
-# Write assignment and if statements here as appropriate.
+if color == "black":
+    charge = charge
+if color == "gold":
+    charge = charge + 15.00
 
-# Output Charge for this sign.
-print("The charge for this sign is $" + str(charge))
+print(f"The charge for this sign is ${charge:.2f}")
+
+#I initialized charge at 35.00 instead of 0.00 because of the set minimum.
+#I edited the print statement to force a two decimal place output from a float instead of using a string.
